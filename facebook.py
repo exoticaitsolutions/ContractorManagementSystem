d93d581b8c8d6ba9f2fcf9257677aabb34bee40f
unknown <webbdeveloper24@gmail.com>
2024-08-05 19:21:21 +0530

    today updated code

--- facebook.py
+++ facebook.py
@@ -2,80 +2,142 @@ import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
-from selenium.webdriver.chrome.options import Options
-
-# import undetected_chromedriver as uc
-
 
 def type_like_human(element, text):
     for character in text:
         element.send_keys(character)
 
+def wait_for_element(driver, by, value, timeout=10):
+    return WebDriverWait(driver, timeout).until(EC.presence_of_element_located((by, value)))
 
-def scrape_twitter_data():
-    # driver = uc.Chrome(use_subprocess=False)
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+def scrape_facebook_data():
     chrome_options = Options()
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--start-maximized")  # Start maximized
     driver = webdriver.Chrome(options=chrome_options)
+    driver.maximize_window()
     driver.get("https://www.facebook.com/")
     sleep(3)
 
     # Enter username
-    username = driver.find_element(By.XPATH, "//input[@name='email']")
+    username = wait_for_element(driver, By.XPATH, "//input[@name='email']")
     ActionChains(driver).move_to_element(username).click().perform()
-    type_like_human(username, "kamal.exoticait@gmail.com")
+    type_like_human(username, "Germniyn Williams")
     print("Username filled successfully")
 
     # Enter password
-    password = driver.find_element(By.ID, "pass")
+    password = wait_for_element(driver, By.ID, "pass")
     ActionChains(driver).move_to_element(password).click().perform()
-    type_like_human(password, "asdf123@")
+    type_like_human(password, "S5Us3/)pT$.H#yy")
     print("Password filled successfully")
 
     # Click login button
-    login_button = driver.find_element(By.CSS_SELECTOR, 'button[data-testid="royal_login_button"]')
+    login_button = wait_for_element(driver, By.CSS_SELECTOR, 'button[data-testid="royal_login_button"]')
     login_button.click()
     sleep(3)
     driver.get("https://www.facebook.com/groups/njcontractorsconnect")
-    sleep(10)
-    # action = ActionChains(driver)
-    # action.move_by_offset(500, 300).click().perform()
-    # sleep(6)
-    link = driver.find_element(By.CSS_SELECTOR, 'a[href="/groups/2020285764874761/members/"]')
+    sleep(4)
+
+    link = wait_for_element(driver, By.CSS_SELECTOR, 'a[href="/groups/2020285764874761/members/"]')
     link.click()
-    sleep(10)
-  
-    driver.execute_script("window.scrollBy(0, window.innerHeight * 0.3);")
-    data = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[4]/div/div/div/div/div/div/div/div/div/div/div[2]')
-    # print("data : ", data.text)
-  
+    sleep(5)
 
-    click_on_profile = data.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[4]/div/div/div/div/div/div/div/div/div/div/div[2]/div[1]/div/div[2]/div/div[1]/div/div/div[2]/div[1]/div/div/div[1]/span/span[1]/span')
-    click_on_profile.click()
-    sleep(7)
-    view_profile = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[5]/div/div/div[3]/div[2]/div[1]/div[1]/div[2]/div/div/div/div[4]/div/div/div[2]/div/a/div/div[1]/div[2]/span/span')
-    view_profile.click()
-    sleep(3)
-    total_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        try:
+            # Scroll down to load more members if necessary
+            driver.execute_script("window.scrollBy(0, window.innerHeight * 0.9);")
+            sleep(5)
 
-    # Calculate the position to scroll to (30% of the total height)
-    scroll_position = total_height * 0.3
+            # Re-find members element and list
+            all_members = wait_for_element(driver, By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[4]/div/div/div/div/div/div/div/div/div/div/div[2]/div[4]/div/div[2]/div')
+            members = all_members.find_elements(By.CLASS_NAME, 'xt0psk2')
+            print("Number of members found:", len(members))
 
-    # Scroll down to the calculated position
-    driver.execute_script(f"window.scrollTo(0, {scroll_position});")
-    sleep(3) 
-    data1 = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[1]/div[2]/div/div[1]/div/div/div/div/div[2]/div[2]')
-    print("Data scraped is :", data1.text)
-    sleep(10)
-    
+            if not members:
+                print("No more members found. Exiting...")
+                break
 
+            for i, member in enumerate(members):
+                try:
+                    print(f"Clicking on member {i+1}")
+                    member.click()
+                    sleep(5)
 
-    
+                    # Scrape data
+                    name = wait_for_element(driver, By.XPATH, '/html/body/div[1]/div/div[1]/div/div[5]/div/div/div[3]/div[2]/div[1]/div[1]/div[2]/div/div/div/div[3]/div/div/div/div[1]/div/div/div/div/div/span').text
+                    print("Name:", name)
 
-scrape_twitter_data()
+                    view_profile = wait_for_element(driver, By.XPATH, '//span[text()="View profile"]')
+                    view_profile.click()
+                    sleep(6)
+
+                    about_link = wait_for_element(driver, By.XPATH, '//a[.//span[text()="About"]]')
+                    about_link.click()
+                    sleep(4)
+
+                    driver.execute_script("window.scrollBy(0, window.innerHeight * 0.5);")
+                    sleep(8)
+
+                    overview = wait_for_element(driver, By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div')
+                    print("Overview:", overview.text)
+
+                    work_and_education_link = wait_for_element(driver, By.XPATH, '//a[contains(@href, "about_work_and_education")]')
+                    work_and_education_link.click()
+                    sleep(5)
+                    scrap_work_and_education_data = wait_for_element(driver, By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div')
+                    print("Work and Education Data:", scrap_work_and_education_data.text)
+
+                    places_lived_link = wait_for_element(driver, By.XPATH, '//a[contains(@href, "about_places")]')
+                    places_lived_link.click()
+                    sleep(5)
+                    scrap_places_lived = wait_for_element(driver, By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div')
+                    print("Places Lived:", scrap_places_lived.text)
+
+                    contact_info_link = wait_for_element(driver, By.XPATH, '//a[contains(@href, "about_contact_and_basic_info")]')
+                    contact_info_link.click()
+                    sleep(5)
+                    scrap_contact_info = wait_for_element(driver, By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div')
+                    print("Contact Info:", scrap_contact_info.text)
+
+                    family_relationships_link = wait_for_element(driver, By.XPATH, '//a[contains(@href, "about_family_and_relationships")]')
+                    family_relationships_link.click()
+                    sleep(5)
+                    scrap_family_relationships = wait_for_element(driver, By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div')
+                    print("Family Relationships:", scrap_family_relationships.text)
+
+                    details_about_zultan_link = wait_for_element(driver, By.XPATH, '//a[contains(@href, "about_details")]')
+                    details_about_zultan_link.click()
+                    sleep(5)
+                    scrap_details_about = wait_for_element(driver, By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div')
+                    print("Details About:", scrap_details_about.text)
+
+                    life_events_link = wait_for_element(driver, By.XPATH, '//a[contains(@href, "about_life_events")]')
+                    life_events_link.click()
+                    sleep(5)
+                    scrap_life_events = wait_for_element(driver, By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]')
+                    print("Life Events:", scrap_life_events.text)
+                    driver.get("https://www.facebook.com/groups/njcontractorsconnect/members")
+                    sleep(3)
+                    driver.execute_script("window.scrollBy(0, window.innerHeight * 0.9);")
+                    sleep(3)
+
+                except Exception as e:
+                    print(f"An error occurred during member data scraping: {e}")
+
+                # Re-find the members list after navigating back
+                # all_members = wait_for_element(driver, By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[4]/div/div/div/div/div/div/div/div/div/div/div[2]/div[4]/div/div[2]/div')
+                # members = all_members.find_elements(By.CLASS_NAME, 'xt0psk2')
+                # print("Refetched members:", len(members))
+
+        except Exception as e:
+            print(f"An error occurred while finding members: {e}")
+            break
+
+    driver.quit()
+
+scrape_facebook_data()
+
